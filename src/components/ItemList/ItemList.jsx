1983a4843ff95a2340e07dde4e2be89f4d63d7cd
Emanuel Valente <ema_zero@hotmail.com>
2022-09-07 17:04:00 -0300

    realizo detalles de css

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -4,11 +4,14 @@ import "./ItemList.css";
 import Item from "../Item/Item";
 import { CircleSpinnerOverlay } from "react-spinner-overlay";
 
+// Recibe el state desde ItemListContainer
 function ItemList({ items }) {
+  // Spinner State
   const [loading, setLoading] = useState(true);
 
   return (
     <div className="cardContainer">
+      {/* Si el array esta completo mapea cada item generando un componente Item*/}
       {items.length ? (
         items.map((item) => (
           <Item
