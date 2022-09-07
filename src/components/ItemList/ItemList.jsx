3a494b81bc841df5d71521872fc647ba830a8147
Emanuel Valente <ema_zero@hotmail.com>
2022-09-07 16:20:50 -0300

    corrijo Desafio - mapeo de items con promesa

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,40 +1,16 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./ItemList.css";
 import Item from "../Item/Item";
 import { CircleSpinnerOverlay } from "react-spinner-overlay";
 
 function ItemList({ items }) {
-  const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
-
-  //componentDidMount
-  // donde un componente necesita hacer algo al montarse es donde se define useEffect.
-  useEffect(() => {
-    let apiPromise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(items);
-
-        // reject("UPS!, Error en el servidor...intente nuevamente");
-      }, 2000);
-    });
-
-    apiPromise
-      .then((res) => {
-        console.log(res);
-        setMovies(res);
-      })
-      .catch((err) => {
-        alert(err);
-      });
-
-    console.log(movies);
-  }, []);
 
   return (
     <div className="cardContainer">
-      {movies.length ? (
-        movies.map((item) => (
+      {items.length ? (
+        items.map((item) => (
           <Item
             key={item.id}
             picture={item.thumbnail}
@@ -50,7 +26,7 @@ function ItemList({ items }) {
           size={42}
           color="rgb(255 158 181)"
           overlayColor="rgb(255 158 181 / 0%)"
-          message={<p className="loadingText">CARGANDO DATOS...</p>}
+          message={<p className="loadingText">ESPERANDO DATOS...</p>}
         />
       )}
     </div>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,150 +1,180 @@
 import React from "react";
 import "./ItemListContainer.css";
 import ItemList from "../ItemList/ItemList";
+import { useState, useEffect } from "react";
 
-function ItemListContainer(props) {
+function ItemListContainer() {
   // const onAdd = (count) => {
   //   alert(`Has agregado ${count} Items al carrito`);
   // };
 
-  const items = [
-    {
-      id: 1,
-      thumbnail: "https://i.postimg.cc/g2StrXps/poster-11.jpg",
-      title: "Tonight she comes",
-      genere: "Terror",
-      premiereDate: "invalid",
-      stars: 5,
-      director: "invalid",
-      type: "movie",
-      price: 300,
-    },
-    {
-      id: 2,
-      thumbnail: "https://i.postimg.cc/1zdd7gYX/poster-12.jpg",
-      title: "It",
-      genere: "Terror",
-      premiereDate: "invalid",
-      stars: 8,
-      director: "invalid",
-      type: "movie",
-      price: 300,
-    },
-    {
-      id: 3,
-      thumbnail: "https://i.postimg.cc/kXCpHPwQ/poster-13.jpg",
-      title: "Joker",
-      genere: "Drama",
-      premiereDate: "invalid",
-      stars: 5,
-      director: "invalid",
-      type: "movie",
-      price: 400,
-    },
-    {
-      id: 4,
-      thumbnail: "https://i.postimg.cc/Y0Ksk57w/poster-14.jpg",
-      title: "Spider-Man Homecoming",
-      genere: "Aventura",
-      premiereDate: "invalid",
-      stars: 7,
-      director: "invalid",
-      type: "movie",
-      price: 500,
-    },
-    {
-      id: 5,
-      thumbnail: "https://i.postimg.cc/Y06ySd2L/poster-15.jpg",
-      title: "Buscando a Nemo",
-      genere: "Animación",
-      premiereDate: "invalid",
-      stars: 8,
-      director: "invalid",
-      type: "movie",
-      price: 400,
-    },
-    {
-      id: 6,
-      thumbnail: "https://i.postimg.cc/fL0rGw6K/poster-16.jpg",
-      title: "John Wick - chapter 2",
-      genere: "Acción",
-      premiereDate: "invalid",
-      stars: 8,
-      director: "invalid",
-      type: "movie",
-      price: 500,
-    },
-    {
-      id: 7,
-      thumbnail: "https://i.postimg.cc/5NmRdKkZ/poster-17.jpg",
-      title: "Interstellar",
-      genere: "Ciencia Ficción",
-      premiereDate: "invalid",
-      stars: 9,
-      director: "Christopher Nolan",
-      type: "movie",
-      price: 300,
-    },
-    {
-      id: 8,
-      thumbnail: "https://i.postimg.cc/Vsnh4k9s/poster-18.jpg",
-      title: "The Nun",
-      genere: "Terror",
-      premiereDate: "invalid",
-      stars: 6,
-      director: "invalid",
-      type: "movie",
-      price: 400,
-    },
-    {
-      id: 9,
-      thumbnail: "https://i.postimg.cc/8C7KvpPS/poster-19.jpg",
-      title: "Jurassic World - Fallen Kingdom",
-      genere: "Ciencia Ficción",
-      premiereDate: "invalid",
-      stars: 9,
-      director: "Steven Spielberg",
-      type: "movie",
-      price: 400,
-    },
-    {
-      id: 10,
-      thumbnail: "https://i.postimg.cc/HksPp0qj/poster-20.jpg",
-      title: "Harry Potter - and the order of the fenix",
-      genere: "Aventura",
-      premiereDate: "invalid",
-      stars: 7,
-      director: "invalid",
-      type: "movie",
-      price: 500,
-    },
-    {
-      id: 11,
-      thumbnail: "https://i.postimg.cc/RFfgPgHp/poster-21.jpg",
-      title: "E.T",
-      genere: "Aventura",
-      premiereDate: "invalid",
-      stars: 7,
-      director: "Steven Spielberg",
-      type: "movie",
-      price: 200,
-    },
-    {
-      id: 12,
-      thumbnail: "https://i.postimg.cc/jjB8QpLJ/poster-22.jpg",
-      title: "The Incredibles",
-      genere: "Animacion",
-      premiereDate: "invalid",
-      stars: 7,
-      director: "invalid",
-      type: "movie",
-      price: 300,
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  //componentDidMount
+  // donde un componente necesita hacer algo al montarse es donde se define useEffect.
+  useEffect(() => {
+    const array = [
+      {
+        id: 1,
+        thumbnail: "https://i.postimg.cc/g2StrXps/poster-11.jpg",
+        title: "Tonight she comes",
+        genere: "Terror",
+        premiereDate: "invalid",
+        stars: 5,
+        director: "invalid",
+        type: "movie",
+        price: 300,
+      },
+      {
+        id: 2,
+        thumbnail: "https://i.postimg.cc/1zdd7gYX/poster-12.jpg",
+        title: "It",
+        genere: "Terror",
+        premiereDate: "invalid",
+        stars: 8,
+        director: "invalid",
+        type: "movie",
+        price: 300,
+      },
+      {
+        id: 3,
+        thumbnail: "https://i.postimg.cc/kXCpHPwQ/poster-13.jpg",
+        title: "Joker",
+        genere: "Drama",
+        premiereDate: "invalid",
+        stars: 5,
+        director: "invalid",
+        type: "movie",
+        price: 400,
+      },
+      {
+        id: 4,
+        thumbnail: "https://i.postimg.cc/Y0Ksk57w/poster-14.jpg",
+        title: "Spider-Man Homecoming",
+        genere: "Aventura",
+        premiereDate: "invalid",
+        stars: 7,
+        director: "invalid",
+        type: "movie",
+        price: 500,
+      },
+      {
+        id: 5,
+        thumbnail: "https://i.postimg.cc/Y06ySd2L/poster-15.jpg",
+        title: "Buscando a Nemo",
+        genere: "Animación",
+        premiereDate: "invalid",
+        stars: 8,
+        director: "invalid",
+        type: "movie",
+        price: 400,
+      },
+      {
+        id: 6,
+        thumbnail: "https://i.postimg.cc/fL0rGw6K/poster-16.jpg",
+        title: "John Wick - chapter 2",
+        genere: "Acción",
+        premiereDate: "invalid",
+        stars: 8,
+        director: "invalid",
+        type: "movie",
+        price: 500,
+      },
+      {
+        id: 7,
+        thumbnail: "https://i.postimg.cc/5NmRdKkZ/poster-17.jpg",
+        title: "Interstellar",
+        genere: "Ciencia Ficción",
+        premiereDate: "invalid",
+        stars: 9,
+        director: "Christopher Nolan",
+        type: "movie",
+        price: 300,
+      },
+      {
+        id: 8,
+        thumbnail: "https://i.postimg.cc/Vsnh4k9s/poster-18.jpg",
+        title: "The Nun",
+        genere: "Terror",
+        premiereDate: "invalid",
+        stars: 6,
+        director: "invalid",
+        type: "movie",
+        price: 400,
+      },
+      {
+        id: 9,
+        thumbnail: "https://i.postimg.cc/8C7KvpPS/poster-19.jpg",
+        title: "Jurassic World - Fallen Kingdom",
+        genere: "Ciencia Ficción",
+        premiereDate: "invalid",
+        stars: 9,
+        director: "Steven Spielberg",
+        type: "movie",
+        price: 400,
+      },
+      {
+        id: 10,
+        thumbnail: "https://i.postimg.cc/HksPp0qj/poster-20.jpg",
+        title: "Harry Potter - and the order of the fenix",
+        genere: "Aventura",
+        premiereDate: "invalid",
+        stars: 7,
+        director: "invalid",
+        type: "movie",
+        price: 500,
+      },
+      {
+        id: 11,
+        thumbnail: "https://i.postimg.cc/RFfgPgHp/poster-21.jpg",
+        title: "E.T",
+        genere: "Aventura",
+        premiereDate: "invalid",
+        stars: 7,
+        director: "Steven Spielberg",
+        type: "movie",
+        price: 200,
+      },
+      {
+        id: 12,
+        thumbnail: "https://i.postimg.cc/jjB8QpLJ/poster-22.jpg",
+        title: "The Incredibles",
+        genere: "Animacion",
+        premiereDate: "invalid",
+        stars: 7,
+        director: "invalid",
+        type: "movie",
+        price: 300,
+      },
+    ];
+
+    const is_OK = Math.random() > 0.5 ? true : false;
+
+    let apiPromise = new Promise((resolve, reject) => {
+      if (is_OK > 0.5) {
+        setTimeout(() => {
+          resolve(array);
+        }, 2000);
+      } else {
+        setTimeout(() => {
+          reject("UPS!, Error en el servidor...recargue la página para reintentar");
+        }, 4000);
+      }
+    });
+
+    // Manejo la respuesta seteando el state con su valor.
+    apiPromise
+      .then((res) => {
+        setMovies(res);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, []);
 
   return (
     <div className="main">
-      <ItemList items={items} />
+      <ItemList items={movies} />
     </div>
   );
 }
